6b950749b8de65caefdc98308e7102a33d17bc62
Adam Courington <adamcourington@gmail.com>
2023-02-24 15:40:06 -0600

    Added Login and register UI

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,61 @@
+import { useForm } from '@mantine/form'
+import {
+  Button,
+  Card,
+  Divider,
+  Stack,
+  TextInput,
+  Title,
+  Anchor,
+} from '@mantine/core'
 function Login() {
-  return <div>Login</div>
+  const loginForm = useForm({
+    initialValues: {
+      name: '',
+      email: '',
+      password: '',
+    },
+  })
+
+  const onSubmit = (event) => {
+    event.preventDefault()
+    console.log(loginForm.values)
+  }
+  return (
+    <div className='flex h-screen justify-center items-center'>
+      <Card
+        sx={{
+          width: 400,
+        }}
+        shadow='lg'
+        withBorder
+      >
+        <form action='' onSubmit={onSubmit}>
+          <Title order={2} mb={5}>
+            LOGIN
+          </Title>
+          <Divider variant='dotted' color='gray' />
+          <Stack mt={5}>
+            <TextInput
+              label='Email'
+              placeholder='Enter your email'
+              name='name'
+              {...loginForm.getInputProps('email')}
+            />
+            <TextInput
+              label='Password'
+              placeholder='Enter your password'
+              name='name'
+              {...loginForm.getInputProps('password')}
+            />
+            <Button type='submit' color='violet'>
+              Login
+            </Button>
+            <Anchor href='/register'>Don't have an account? Register</Anchor>
+          </Stack>
+        </form>
+      </Card>
+    </div>
+  )
 }
 export default Login
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,6 +1,67 @@
+import { useForm } from '@mantine/form'
+import {
+  Button,
+  Card,
+  Divider,
+  Stack,
+  TextInput,
+  Title,
+  Anchor,
+} from '@mantine/core'
 function Register() {
+  const registerForm = useForm({
+    initialValues: {
+      name: '',
+      email: '',
+      password: '',
+    },
+  })
+
+  const onSubmit = (event) => {
+    event.preventDefault()
+    console.log(registerForm.values)
+  }
   return (
-    <div>Register</div>
+    <div className='flex h-screen justify-center items-center'>
+      <Card
+        sx={{
+          width: 400,
+        }}
+        shadow='lg'
+        withBorder
+      >
+        <form action='' onSubmit={onSubmit}>
+          <Title order={2} mb={5}>
+            REGISTER
+          </Title>
+          <Divider variant='dotted' color='gray' />
+          <Stack mt={5}>
+            <TextInput
+              label='Name'
+              placeholder='Enter your name'
+              name='name'
+              {...registerForm.getInputProps('name')}
+            />
+            <TextInput
+              label='Email'
+              placeholder='Enter your email'
+              name='name'
+              {...registerForm.getInputProps('email')}
+            />
+            <TextInput
+              label='Password'
+              placeholder='Enter your password'
+              name='name'
+              {...registerForm.getInputProps('password')}
+            />
+            <Button type='submit' color='violet'>
+              Register
+            </Button>
+            <Anchor href='/login'>Allready have an account? Login</Anchor>
+          </Stack>
+        </form>
+      </Card>
+    </div>
   )
 }
 export default Register
